f0a3d4ae98c9977f1e4ec24b89baaacee77f49df
Hanzala-Ahmed <hanzala.ahmed181@gmail.com>
2021-06-06 13:49:37 +0500

    Add

--- 01-Variables/index.js
+++ 01-Variables/index.js
@@ -1,3 +1,11 @@
-let name = "Hanzala";
+let nam = "Hanzala";
 
-console.log(name);
+console.log(nam);
+
+var fruit = ["apple", "mango", "banana"]
+
+console.log(fruit);
+
+console.log(fruit[0],fruit[1],fruit[2]);
+
+for
